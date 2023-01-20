2848df90182f4dfd08e4acc68a6a248e393e98b9
AdrianFredes <adrian.fredes.maldonado@gmail.com>
2023-01-20 11:17:49 -0300

    comentarios prueba

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -1,3 +1,4 @@
+// mensaje comentario en contacto
 $(document).ready(function(){
     $("#send").click(function(){
         alert("¡Gracias! Revisaremos tu mensaje y nuestro equipo se contactará contigo.");
